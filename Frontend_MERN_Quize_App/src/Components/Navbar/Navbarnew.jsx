2acccf015502b397f7023d48014d22e2d630bcc8
Bipin-2022 <bipinmadesiya799@gmail.com>
2024-06-24 10:16:47 +0530

    v2

--- Frontend_MERN_Quize_App/src/Components/Navbar/Navbarnew.jsx
+++ Frontend_MERN_Quize_App/src/Components/Navbar/Navbarnew.jsx
@@ -42,7 +42,6 @@ export const Navbarnew = () => {
       </Link>
 
       <div className="w-2/5 ml-14 ">
-        <img className="w-96 h-24" src="./MERN2.png" alt="" />
         {/* <div className="flex h-10 py-2 mt-1 bg-gray-200">
           <div className="w-3/4 ">
             <input
